7e010eb3ee88cf06ffcf044a66cbffd81e70e446
sohamkamani <sohamkamani@gmail.com>
2023-03-19 16:15:37 +0530

    fix indent

--- src/main/java/com/sohamkamani/cloudsql/App.java
+++ src/main/java/com/sohamkamani/cloudsql/App.java
@@ -5,13 +5,18 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
-        DataSource dataSource = CloudSqlConnectionPoolFactory.createConnectionPool();
-        ResultSet rs =
-                dataSource.getConnection().prepareStatement("select * from birds").executeQuery();
-        while (rs.next()) {
-            System.out.println("name: " + rs.getString("bird") + " description:"
-                    + rs.getString("description"));
-        }
+  public static void main(String[] args) throws SQLException {
+    // Get a new datasource from the method we defined before
+    DataSource dataSource = CloudSqlConnectionPoolFactory.createConnectionPool();
+
+    // Run a query and get the result
+    ResultSet rs =
+        dataSource.getConnection().prepareStatement("select * from birds").executeQuery();
+
+    // print the results to the console
+    while (rs.next()) {
+      System.out
+          .println("name: " + rs.getString("bird") + " description:" + rs.getString("description"));
     }
+  }
 }
--- src/main/java/com/sohamkamani/cloudsql/CloudSqlConnectionPoolFactory.java
+++ src/main/java/com/sohamkamani/cloudsql/CloudSqlConnectionPoolFactory.java
@@ -5,49 +5,26 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class CloudSqlConnectionPoolFactory {
-    // Note: Saving credentials in environment variables is convenient, but not
-    // secure - consider a more secure solution such as
-    // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    // keep secrets safe.
-    private static final String INSTANCE_CONNECTION_NAME =
-            "sohamkamani-website:us-central1:my-sample-database";
-    private static final String DB_USER = "postgres");
-    private static final String DB_PASS = "<password>");
-    private static final String DB_NAME = "bird_encyclopedia");
+  // I'm defining my database credentials here for convenience
+  // but in practice, you should use environment variables
+  // (https://docs.oracle.com/javase/tutorial/essential/environment/env.html)
+  // or Cloud Secret Manager (https://cloud.google.com/secret-manager)
+  private static final String INSTANCE_CONNECTION_NAME =
+      "sohamkamani-website:us-central1:my-sample-database";
+  private static final String DB_USER = "postgres";
+  private static final String DB_PASS = "<password>";
+  private static final String DB_NAME = "bird_encyclopedia";
 
-    public static DataSource createConnectionPool() {
-        // The configuration object specifies behaviors for the connection pool.
-        HikariConfig config = new HikariConfig();
+  public static DataSource createConnectionPool() {
+    // create a new configuration and set the database credentials
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(String.format("jdbc:postgresql:///%s", DB_NAME));
+    config.setUsername(DB_USER);
+    config.setPassword(DB_PASS);
+    config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
+    config.addDataSourceProperty("cloudSqlInstance", INSTANCE_CONNECTION_NAME);
 
-        // The following URL is equivalent to setting the config options below:
-        // jdbc:postgresql:///<DB_NAME>?cloudSqlInstance=<INSTANCE_CONNECTION_NAME>&
-        // socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=<DB_USER>&password=<DB_PASS>
-        // See the link below for more info on building a JDBC URL for the Cloud SQL JDBC Socket
-        // Factory
-        // https://github.com/GoogleCloudPlatform/cloud-sql-jdbc-socket-factory#creating-the-jdbc-url
-
-        // Configure which instance and what database user to connect with.
-        config.setJdbcUrl(String.format("jdbc:postgresql:///%s", DB_NAME));
-        config.setUsername(DB_USER); // e.g. "root", _postgres"
-        config.setPassword(DB_PASS); // e.g. "my-password"
-
-        config.addDataSourceProperty("socketFactory",
-                "com.google.cloud.sql.postgres.SocketFactory");
-        config.addDataSourceProperty("cloudSqlInstance", INSTANCE_CONNECTION_NAME);
-
-        // [END cloud_sql_postgres_servlet_connect_connector]
-        // [START cloud_sql_postgres_servlet_connect_connector]
-
-        // [END cloud_sql_postgres_servlet_connect_unix]
-        // The ipTypes argument can be used to specify a comma delimited list of preferred IP types
-        // for connecting to a Cloud SQL instance. The argument ipTypes=PRIVATE will force the
-        // SocketFactory to connect with an instance's associated private IP.
-        config.addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
-        // [START cloud_sql_postgres_servlet_connect_unix]
-
-
-
-        // Initialize the connection pool using the configuration object.
-        return new HikariDataSource(config);
-    }
+    // Initialize the connection pool using the configuration object.
+    return new HikariDataSource(config);
+  }
 }
