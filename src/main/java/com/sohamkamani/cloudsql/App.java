00b31607e55e495433a5a5349ac7f4eb66fd9a47
sohamkamani <sohamkamani@gmail.com>
2023-06-03 13:34:08 +0530

    added cloud storage examples

--- src/main/java/com/sohamkamani/cloudsql/App.java
+++ src/main/java/com/sohamkamani/cloudsql/App.java
@@ -6,14 +6,11 @@ import javax.sql.DataSource;
 
 public class App {
   public static void main(String[] args) throws SQLException {
-    // Get a new datasource from the method we defined before
     DataSource dataSource = CloudSqlConnectionPoolFactory.createConnectionPool();
 
-    // Run a query and get the result
     ResultSet rs =
         dataSource.getConnection().prepareStatement("select * from birds").executeQuery();
 
-    // print the results to the console
     while (rs.next()) {
       System.out
           .println("name: " + rs.getString("bird") + " description:" + rs.getString("description"));
